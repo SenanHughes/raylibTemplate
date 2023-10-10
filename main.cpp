f5e2713c566d312f602d9aac0095b2969d707a47
SenanHughes <senan_hughes@icloud.com>
2023-10-10 21:08:13 +0100

    MarioMoveExample

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@ int main() {
     const int screenHeight = 600;
 
     // Initialize the Window
-    InitWindow(screenWidth, screenHeight, "Conditionals");
+    InitWindow(screenWidth, screenHeight, "Input Movement Example");
 
-    bool isEven = false;
-    int number = 6;
-        
-    if (number%2 == 0){
-        isEven = true;
-    }
+    Texture2D marioTexture = LoadTexture("Resources/Textures/mario.png");
+
+    Vector2 scale = {0.25f, 0.25f};
+
+    float rotation = 0.0f;
+    Color tint = WHITE;
+    
+    Vector2 position = {(screenWidth - (marioTexture.width * scale.x))/2, (screenHeight-(marioTexture.height * scale.y))/2};
+    
+    const float moveSpeed = 5.0f;
+
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
@@ -21,21 +26,29 @@ int main() {
     // The Game Loop
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
+        if(IsKeyDown(KEY_RIGHT)) position.x += moveSpeed;
+        if(IsKeyDown(KEY_LEFT)) position.x -= moveSpeed;
+        if(IsKeyDown(KEY_UP)) position.y += moveSpeed;
+        if(IsKeyDown(KEY_DOWN)) position.y -= moveSpeed;
+
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) rotation += 10.0f;
+        if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) rotation -= 10.0f;
         // Setup Canvas
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(PINK);
 
         // Here goes all the Game Logic
-        if(isEven){
-            DrawText("Number is even", 10, 10, 20, BLUE);
-        }else{
-            DrawText("Number is odd", 10, 10, 20, BLUE);
-        }
+        DrawTextureEx(marioTexture, position, rotation, 0.5f, tint);
+
+
 
         // teardown Canvas
         EndDrawing();
     }
+
+    UnloadTexture(marioTexture);
+
     CloseWindow();
     return 0;
 }
